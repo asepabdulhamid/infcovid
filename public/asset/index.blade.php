da2e7db24c5660dd2593f5f4f55e635fc1be963b
asepabdulhamid <badneet25@gmail.com>
2020-09-30 15:20:27 +0700

    peroject pertama

---
+++ public/asset/index.blade.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<!--
+Template Name: Oleald
+Author: <a href="https://www.os-templates.com/">OS Templates</a>
+Author URI: https://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: https://www.os-templates.com/template-terms
+-->
+<html lang="">
+<!-- To declare your language - read more here: https://www.w3.org/International/questions/qa-html-language-declarations -->
+<head>
+<title>Oleald</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="/dist/css/bootstrap.min.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay light" style="background-image:url('images/demo/backgrounds/01.png');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div class="fl_left"> 
+        <!-- ################################################################################################ -->
+        <ul class="nospace">
+          <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
+          <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </div>
+      <div class="fl_right"> 
+        <!-- ################################################################################################ -->
+        <ul class="nospace">
+          <li><a href="index.html"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="#" title="Help Centre"><i class="fa fa-life-bouy"></i></a></li>
+          <li><a href="#" title="Login"><i class="fa fa-lg fa-sign-in"></i></a></li>
+          <li><a href="#" title="Sign Up"><i class="fa fa-lg fa-edit"></i></a></li>
+        </ul>
+        <!-- ################################################################################################ -->
+      </div>
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html">Oleald</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.html">Home</a></li>
+          <li><a class="drop" href="#">Pages</a>
+            <ul>
+              <li><a href="pages/gallery.html">Gallery</a></li>
+              <li><a href="pages/full-width.html">Full Width</a></li>
+              <li><a href="pages/sidebar-left.html">Sidebar Left</a></li>
+              <li><a href="pages/sidebar-right.html">Sidebar Right</a></li>
+              <li><a href="pages/basic-grid.html">Basic Grid</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Dropdown</a>
+            <ul>
+              <li><a href="#">Level 2</a></li>
+              <li><a class="drop" href="#">Level 2 + Drop</a>
+                <ul>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                  <li><a href="#">Level 3</a></li>
+                </ul>
+              </li>
+              <li><a href="#">Level 2</a></li>
+            </ul>
+          </li>
+          <li><a href="#">Link Text</a></li>
+          <li><a href="#">Link Text</a></li>
+        </ul>
+      </nav>
+      <!-- ################################################################################################ -->
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article>
+      <h3 class="heading">Sollicitudin<br>
+        ligula cras lobortis tortor</h3>
+      <p>Et lorem pulvinar consectetur suspendisse nec imperdiet mi sed ac porttitor nisl nam viverra diam at lectus malesuada mattis morbi quis.</p>
+      <footer><a class="btn" href="#">Bibendum tellus nullam</a></footer>
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="group excerpt">
+      <article class="one_third first"><a class="imgover btmspace-30" href="#"><img src="images/demo/320x240.png" alt=""></a>
+        <h6 class="heading">Sapien lorem interdum</h6>
+        <p>Purus in ornare feugiat ex vivamus bibendum augue nisl sit amet dictum quam aliquet id phasellus interdum leo sit amet&hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="images/demo/320x240.png" alt=""></a>
+        <h6 class="heading">Convallis vulputate mi</h6>
+        <p>Sem vestibulum lacus sit amet eleifend velit tellus quis ligula nam egestas eros sit amet vulputate porttitor proin leo&hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="images/demo/320x240.png" alt=""></a>
+        <h6 class="heading">Turpis suscipit vitae magna</h6>
+        <p>At pretium tempus erat ut vel hendrerit velit aliquam feugiat eget velit eu ultrices vivamus mauris leo aenean sagittis&hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row2 bgded overlay" style="background-image:url('images/demo/backgrounds/02.png');">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">Porta consequat cras imperdiet</h6>
+      <p>Lobortis erat at sodales libero malesuada vel curabitur maximus</p>
+    </div>
+    <ul class="nospace group overview">
+      <li class="one_third">
+        <article><a href="#"><i class="fa fa-headphones"></i></a>
+          <h6 class="heading"><a href="#">Arcu eleifend venenatis</a></h6>
+          <p>Sed non vulputate arcu nullam eu massa vitae urna vulputate efficitur in et&hellip;</p>
+          <footer><a href="#">View Details &raquo;</a></footer>
+        </article>
+      </li>
+      <li class="one_third">
+        <article><a href="#"><i class="fa fa-asl-interpreting"></i></a>
+          <h6 class="heading"><a href="#">Metus porta tincidunt</a></h6>
+          <p>Mauris eu tristique aenean dapibus facilisis risus sed vestibulum sed vitae&hellip;</p>
+          <footer><a href="#">View Details &raquo;</a></footer>
+        </article>
+      </li>
+      <li class="one_third">
+        <article><a href="#"><i class="fa fa-area-chart"></i></a>
+          <h6 class="heading"><a href="#">Lacus pulvinar euismod</a></h6>
+          <p>Pulvinar nulla elementum massa ut fermentum accumsan nulla luctus at risus&hellip;</p>
+          <footer><a href="#">View Details &raquo;</a></footer>
+        </article>
+      </li>
+      <li class="one_third">
+        <article><a href="#"><i class="fa fa-balance-scale"></i></a>
+          <h6 class="heading"><a href="#">Efficitur aliquet morbi</a></h6>
+          <p>Vitae est eu felis ultrices tristique morbi eget libero ultricies semper enim&hellip;</p>
+          <footer><a href="#">View Details &raquo;</a></footer>
+        </article>
+      </li>
+      <li class="one_third">
+        <article><a href="#"><i class="fa fa-comments-o"></i></a>
+          <h6 class="heading"><a href="#">Eget tempor interdum</a></h6>
+          <p>Et malesuada fames ac ante ipsum primis in faucibus duis volutpat gravida nisi&hellip;</p>
+          <footer><a href="#">View Details &raquo;</a></footer>
+        </article>
+      </li>
+      <li class="one_third">
+        <article><a href="#"><i class="fa fa-gears"></i></a>
+          <h6 class="heading"><a href="#">Cursus egestas curabitur</a></h6>
+          <p>Aliquam lorem lorem ac iaculis eros pulvinar nec vivamus vestibulum nisl sem&hellip;</p>
+          <footer><a href="#">View Details &raquo;</a></footer>
+        </article>
+      </li>
+    </ul>
+    <footer class="center"><a class="btn" href="#">Quis vehicula nulla</a></footer>
+    <!-- ################################################################################################ -->
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<
---
+++ resources/views/index.blade.php
@@ -0,0 +1,98 @@
+<html>
+<head>
+<tittle></tittle>
+</head>
+<body>
+<html lang="">
+<!-- To declare your language - read more here: https://www.w3.org/International/questions/qa-html-language-declarations -->
+<head>
+<title>INFCOVID</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="/asset/layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay light" style="background-image:url('asset/images/covid.jpeg');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row0">
+    <div id="topbar" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+        <!-- ################################################################################################ -->
+        <!-- ################################################################################################ -->
+      <!-- ################################################################################################ -->
+    </div>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html">INFCOVID</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="/login">LOGOUT</a></li>
+          <li><a class="active" href="index.html">HOME</a>
+          <li><a class="active" href="/layout">ADMIN</a></li> 
+           
+        </ul>
+      </nav>
+      <!-- ################################################################################################ -->
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <div id="pageintro" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <article>
+    <header
+      background-image url="covid.jpeg"></background-image>
+    
+      <h4 class="heading">INFCOVID<br>
+        </h4>
+        <h3>SELURUH RUMAH SAKIT BANDAR LAMPUNG YANG MENYEDIAKAN TEST SWAB & RAPID TEST :) </h3>
+    </article>
+    <!-- ################################################################################################ -->
+  </div>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row3">
+  <main class="hoc container clear"> 
+    <!-- main body -->
+    <!-- ################################################################################################ -->
+    <div class="group excerpt">
+      <article class="one_third first"><a class="imgover btmspace-30" href="#"><img src="asset/images/rs imanuel.webp" alt=""></a>
+        <h6 class="heading">RUMAH SAKIT IMANUEL</h6>
+        <p>rumah sakit ini sudah menyediakan test swab dan rapid test &hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="asset/images/rs pertamina.jpg" alt=""></a>
+        <h6 class="heading">RUMAH SKIT PERTAMINA </h6>
+        <p>RUMAH sakit ini juga sudah menyediakan test swab dan rpid test &hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+      <article class="one_third"><a class="imgover btmspace-30" href="#"><img src="asset/images/rs sukabudi.jpg" alt=""></a>
+        <h6 class="heading">RUMAH SAKIT SUKAWAYANA</h6>
+        <p>rumah sakit yang berda di bandar lampung ini juga menyediakn test swab dan rapid test &hellip;</p>
+        <footer><a href="#">Service Details &raquo;</a></footer>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+    <!-- / main body -->
+    <div class="clear"></div>
+  </main>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
